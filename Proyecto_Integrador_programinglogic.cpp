011175cb2d21235f46ff3c2e18e4441170fdde51
Marina Rivera Vazquez <156823314+Aceituno4488@users.noreply.github.com>
2024-11-18 14:31:23 -0400

    Update Proyecto_Integrador_programinglogic.cpp

--- Proyecto_Integrador_programinglogic.cpp
+++ Proyecto_Integrador_programinglogic.cpp
@@ -1,290 +1,209 @@
-/*ACTUALIZACION: Ya hice el cuestionario de attack on tittan y le di mejor estructura a el cuestionario de one piece....
-Cosas que faltarian: El score, Mejor ortografia en el codigo, y cuando por fin finalicemos con este, necesitamos escribir en comentarios
-nuestros nombres en cada parte de el codigo donde aportamos...tambien una breve explicacion de lo que hicimos expecificamente en esa parte
-de el codigo...*/
+/*ACTUALIZACION: Mejore la estructura de el codigo para que no sea tan nry, para que no de repita tanto...*/
 
 #include <iostream>
 using namespace std;
 
-// funcion para mostrar el menu
+// Definicion de la funcion para hacer preguntas
+void hacer_pregunta(string pregunta, string opciones[], char respuesta_correcta) {
+    cout << pregunta << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << opciones[i] << endl;  // Mostrar las opciones
+    }
+
+    char respuesta_usuario;
+    cout << "Tu respuesta: ";
+    cin >> respuesta_usuario;
+
+    // Verificar la respuesta
+    if (respuesta_usuario == respuesta_correcta) {
+        cout << "¡Correcto!" << endl;
+    } else {
+        cout << "Incorrecto. La respuesta correcta es: " << respuesta_correcta << endl;
+    }
+}
+
+// Definicion de la funcion que muestra el menu principal
 void mostrar_menu() {
-	cout << "  \n                          Misterio Anime!!! \n";
-	cout << "________________________________________________________________________________ \n";
-	cout << "\n    Elige una de las opciones: \n"
-	     << "    " << "1: Naruto"
-	     << "    " << "2: One Piece"
-	     << "    " << "3: Attack on Titan" //si desean pueden poner aun mas opciones :D
-	     << "    " << "4: Cuestionario"
-	     << "    " << "5: Salir \n"
-	     << "    " << "\n    Ingresa tu eleccion: \n ";
+    cout << "\n                    AnimeTrivia\n";
+    cout << "_________________________________________________________\n";
+    cout << "1. Historia del Anime  |  2. Cuestionarios  |  3. Salir\n";
+    cout << "\n Seleccione una opcion: ";
 }
 
-//Este es el menu de el cuestonario
-void cuestionario_menu() {
-	cout << "\n                              Cuestionario Anime!!! \n";
-	cout << "________________________________________________________________________________ \n";
-	cout << "\n    Elige el anime del cuestionario: \n"
-	     << "    1: Naruto"
-	     << "    2: One Piece"
-	     << "    3: Attack on Titan"
-	     << "    4: Regresar al menu: principal\n"
-	     << "\n    Ingresa tu eleccicion: \n";
-}
-
-//Preguntas de cuestionario
+// Funciones de cuestionarios para cada anime
 void cuestionario_naruto() {
-	cout << "\n                              Cuestionario sobre Naruto \n";
-	cout << "________________________________________________________________________________ \n";
-	cout << "\n 1. B?Como se llama el equipo al que pertenece Naruto? \n"
-	     << "    " << "(a)Equipo 8"
-	     << "    " << "(b)Equipo 10"
-	     << "    " << "(c)Equipo 7"
-	     << "    " << "(d)Equipo 3\n";
+    cout << "\n                              Cuestionario sobre Naruto \n";
+    cout << "________________________________________________________________________________ \n";
 
-	cout << "    Conteste (a, b, c, d): ";
-	char respuesta;
-	cin >> respuesta;
-	if (respuesta == 'c') {
-		cout << "    B!Correcto! El equipo de Naruto es el Equipo 7.\n";
-	}
-	else {
-		cout << "Incorrecto. La respuesta correcta es: Equipo 7.\n";
-	}
+    string opciones_de_respuesta[][4] = {
+        {"(a) Equipo 8", "(b) Equipo 10", "(c) Equipo 7", "(d) Equipo 3"},
+        {"(a) Convertirse en Hokage", "(b) Dominar todas las tecnicas ninja", "(c) Superar a Sasuke", "(d) Convertirse en un maestro en taijutsu"},
+        {"(a) Sasuke Hatake", "(b) Sasuke Hyuga", "(c) Sasuke Uchiha", "(d) Sasuke Uzumaki"}
+    };
 
-	// Pausa hasta que el usuario presione Enter
-	cout << "             \nPresione Enter para continuar...\n";
-	cin.ignore(); // Ignora el salto de el enter pendiente
-	cin.get();    // Espera a que se presione Enter
+    string preguntas[] = {
+        "¿Como se llama el equipo al que pertenece Naruto?",
+        "¿Cual es el sueño de Naruto Uzumaki?",
+        "¿Cual es el nombre completo de Sasuke?"
+    };
 
-	cout << "\n______________________________________________________________________________ \n";
-	cout << "\n 2. B?CuC!l es el sueC1o de Naruto Uzumaki?\n"
-	     << "    " << "(a) Convertirse en Hokage"
-	     << "    " << "(b) Dominar todas las tC)cnicas ninja"
-	     << "    " << "(c) Superar a Sasuke"
-	     << "    " << "(d) Convertirse en un maestro en taijutsu\n";
+    char respuestas[] = {'c', 'a', 'c'};
 
-	cout << "     Conteste (a, b, c, d): \n" ;
-	cin >> respuesta;
-	if (respuesta == 'a') {
-		cout << "\n    Correcto!, Queria convertirse en Hokage.\n";
-	}
-	else {
-		cout << "\n Incorrecto...Queria convertirse en Hokage.\n";
-	}
-
-	cout << "             \nPresione Enter para continuar...\n";
-	cin.ignore(); // Ignora el salto de el enter pendiente
-	cin.get();    // Espera a que se presione Enter
-
-	cout << "\n______________________________________________________________________________ \n";
-	cout << "\n 3. B?CuC!l es el nombre completo de Sasuke?\n"
-	     << "    " << "(a) Sasuke Hatake"
-	     << "    " << "(b) Sasuke HyE+ga"
-	     << "    " << "(c) Sasuke Uchiha"
-	     << "    " << "(d) Sasuke Uzumaki \n";
-
-	cout << "      Conteste (a, b, c, d): \n" ;
-	cin >> respuesta;
-	if (respuesta == 'c') {
-		cout << "\nCorrecto!, La respuesta era Sasuke Uchiha.\n";
-	}
-	else {
-		cout << "\n Incorrecto, su nombre completo es Sasuke Uchiha.\n";
-
-	}
-
-	cout << "\n______________________________________________________________________________ \n";
-	cout << "\n 2.B?QuC) tipo de tC)cnica utiliza Kakashi con su Sharingan?\n"
-	     << "    " << "(a) Amaterasu"
-	     << "    " << "(b) Kamui"
-	     << "    " << "(c) Izanagi"
-	     << "    " << "(d) Izanami";
-
-	cout << "     Conteste (a, b, c, d): \n" ;
-	cin >> respuesta;
-	if (respuesta == 'b') {
-		cout << "\n    Correcto!, la tecnica utilizada de kakashi es Kamui\n";
-	}
-	else {
-		cout << "\n Incorrecto, la respuesta es Kamui\n";
-	}
-
-	// Pausa hasta que el usuario presione Enter
-	cout << "             \nPresione Enter para regresar al menu del cuestionario...";
-	cin.ignore(); // Ignora el salto de el enter pendiente
-	cin.get();    // Espera a que se presione Enter
+    for (int i = 0; i < 3; i++) {
+        hacer_pregunta(preguntas[i], opciones_de_respuesta[i], respuestas[i]);
+        
+        cout << "\n Presiona enter para continuar...\n";
+        cin.ignore();
+        cin.get();
+    }
 }
 
 void cuestionario_onepiece() {
-	cout << "\n                              Cuestionario sobre One Piece \n";
-	cout << "________________________________________________________________________________ \n";
-	cout << "\n 1. B?QuiC)n fue el primer miembro en unirse a la tripulaciC3n de los Sombrero de Paja?\n";
-	cout << "    " << "(a) Sanji";
-	cout << "    " << "(b) Nami";
-	cout << "    " << "(c) Usopp";
-	cout << "    " << "(d) Zoro\n";
+    cout << "\n                              Cuestionario sobre One Piece \n";
+    cout << "________________________________________________________________________________ \n";
 
-	cout <<     "Conteste (a, b, c, d): ";
-	char respuesta;
-	cin >> respuesta;
-	if (respuesta == 'd') {
-		cout << "    Correcto! El primer miembro fue Zoro.\n";
-	}
-	else {
-		cout << "Incorrecto. La respuesta correcta es: Zoro.\n";
-	}
+    string opciones_de_respuesta[][4] = {
+        {"(a) Zoro", "(b) Luffy", "(c) Sanji", "(d) Nami"},
+        {"(a) El One Piece", "(b) El tesoro de Gold Roger", "(c) La Gran Piramide", "(d) La Isla del Tesoro"},
+        {"(a) Barbanegra", "(b) Ace", "(c) Shanks", "(d) Kizaru"}
+    };
 
-	// Pausa hasta que el usuario presione Enter
-	cout << "\nPresione Enter para continuar \n...";
-	cin.ignore();
-	cin.get();
+    string preguntas[] = {
+        "¿Quien es el primero en unirse a la tripulacion de Luffy?",
+        "¿Que es el 'One Piece'?",
+        "¿Quien es el principal enemigo de Luffy durante el arco de Marineford?"
+    };
+
+    char respuestas[] = {'b', 'a', 'a'};
+
+    for (int i = 0; i < 3; i++) {
+        hacer_pregunta(preguntas[i], opciones_de_respuesta[i], respuestas[i]);
+        
+        cout << "\n Presiona enter para continuar...\n";
+        cin.ignore();
+        cin.get();
+    }
 }
 
 void cuestionario_attack_on_titan() {
-	cout << "\n                              Cuestionario sobre Attack on Titan \n";
-	cout << "________________________________________________________________________________ \n";
+    cout << "\n                              Cuestionario sobre Attack on Titan \n";
+    cout << "________________________________________________________________________________ \n";
 
-	cout << "\n 1. ¿Cómo se llama el protagonista principal de Attack on Titan?\n"
-	     << "    " << "(a) Armin Arlert"
-	     << "    " << "(b) Mikasa Ackerman"
-	     << "    " << "(c) Eren Yeager"
-	     << "    " << "(d) Levi Ackerman\n";
+    string opciones_de_respuesta[][4] = {
+        {"(a) Eren Yeager", "(b) Mikasa Ackerman", "(c) Levi Ackerman", "(d) Armin Arlert"},
+        {"(a) El muro Maria", "(b) El muro Sina", "(c) El muro Rose", "(d) El muro Levi"},
+        {"(a) La humanidad esta atrapada por titanes", "(b) Los titanes son los humanos", "(c) Hay un traidor entre los soldados", "(d) Los titanes son invisibles"}
+    };
 
-	cout << "Conteste (a, b, c, d): ";
-	char respuesta;
-	cin >> respuesta;
-	if (respuesta == 'c') {
-		cout << "    ¡Correcto! El protagonista principal es Eren Yeager.\n";
-	} else {
-		cout << "Incorrecto. La respuesta correcta es: Eren Yeager.\n";
-	}
+    string preguntas[] = {
+        "¿Quien es el protagonista principal de la serie?",
+        "¿Cual es el primer muro que cae durante el ataque de los titanes?",
+        "¿Que evento inicia la lucha contra los titanes?"
+    };
 
-	cout << "             \nPresione Enter para continuar...\n";
-	cin.ignore();
-	cin.get();
+    char respuestas[] = {'a', 'b', 'a'};
 
-	cout << "\n______________________________________________________________________________ \n";
-	cout << "\n 2. ¿Qué tipo de titán es Eren Yeager inicialmente?\n"
-	     << "    " << "(a) Titán de Ataque"
-	     << "    " << "(b) Titán Bestia"
-	     << "    " << "(c) Titán Colosal"
-	     << "    " << "(d) Titán Fundador\n";
-
-	cout << "Conteste (a, b, c, d): ";
-	cin >> respuesta;
-	if (respuesta == 'a') {
-		cout << "\n    ¡Correcto! Eren Yeager posee el Titán de Ataque al principio.\n";
-	} else {
-		cout << "\n Incorrecto. La respuesta correcta es: Titán de Ataque.\n";
-	}
-
-	cout << "             \nPresione Enter para continuar...\n";
-	cin.ignore();
-	cin.get();
-
-	cout << "\n______________________________________________________________________________ \n";
-	cout << "\n 3. ¿Quién es conocido como el \"soldado más fuerte de la humanidad\"?\n"
-	     << "    " << "(a) Eren Yeager"
-	     << "    " << "(b) Mikasa Ackerman"
-	     << "    " << "(c) Levi Ackerman"
-	     << "    " << "(d) Erwin Smith\n";
-
-	cout << "Conteste (a, b, c, d): ";
-	cin >> respuesta;
-	if (respuesta == 'c') {
-		cout << "\n    ¡Correcto! Levi Ackerman es el \"soldado más fuerte de la humanidad\".\n";
-	} else {
-		cout << "\n Incorrecto. La respuesta correcta es: Levi Ackerman.\n";
-	}
-	
-		// Pausa hasta que el usuario presione Enter
-			cout << "    \nPresione Enter para regresar al menu del cuestionario...";
-			cin.ignore();
-			cin.get();
+    for (int i = 0; i < 3; i++) {
+        hacer_pregunta(preguntas[i], opciones_de_respuesta[i], respuestas[i]);
+        
+        cout << "\n Presiona enter para continuar...\n";
+        cin.ignore();
+        cin.get();
+    }
 }
 
-
-//aqui se controla el cuestionario
-void cuestonario_manage() {
-	int eleccion_cuestionario;
-	do {
-		cuestionario_menu();
-		cin >> eleccion_cuestionario;
-
-		switch (eleccion_cuestionario) {
-		case 1:
-			cuestionario_naruto();
-			break;
-		case 2:
-			cuestionario_onepiece();
-			break;
-
-		case 3:
-			cuestionario_attack_on_titan();
-			break;
-
-		case 4:
-			cout <<"Regresando al menC: principal...\n";
-			break;
-
-		default:
-			cout << "OpciC3n no vC!lida. Por favor, intente de nuevo.\n";
-			break;
-		}
-	} while (eleccion_cuestionario != 4);
+// Funciones para ver historias de los animes
+void mostrar_historia_naruto() {
+    cout << "\nHistoria de Naruto:\n";
+    cout << "Naruto Uzumaki es un joven ninja que lucha por ganarse el respeto de sus compañeros y encontrar su lugar en el mundo. Su sueño es convertirse en Hokage, el lider de su aldea, y proteger a todos los que ama. La serie sigue su crecimiento como ninja y sus batallas contra amenazas peligrosas.\n";
+    cout << "\n Presiona enter para continuar...\n";
+    cin.ignore();
+    cin.get();
 }
 
-
-// swish para opciones...FALTA LA INFORMACION DE LOS ANIMES
-void opcion_menu(int opcion) {
-	switch (opcion) {
-	case 1:
-		cout << "\n opcion 1: Naruto.\n";
-		cout << "\n Personajes: Naruto, Sasuke, Sakura, Kakashi\n";
-		cout << "\n Info: Naruto sigue la historia de un joven ninja rechazado por su aldea que, con esfuerzo y amistad, busca convertirse en Hokage mientras enfrenta poderosos enemigos y descubre los secretos de su mundo. \n";
-		break;
-	case 2:
-		cout << "\n opcion 2: One Piece.\n";
-		cout << "\n Personajes: Luffy, Zoro, Nami, Usopp, Sanji\n";
-		cout << "\n Info: One Piece narra las aventuras de Monkey D. Luffy y su tripulaciC3n en busca del tesoro legendario \n";
-		cout << " para convertirse en el Rey de los Piratas, enfrentando enemigos y explorando un mundo lleno de misterios.\n";
-		break;
-	case 3:
-		cout << "\n opcion 3: Attack on Titan.\n";
-		cout << "\n Personajes: Eren Yeager, Mikasa Ackerman, Armin Arlert, Levi Ackerman\n";
-		cout << "\n Info: Attack on Titan sigue a la humanidad, que vive en ciudades rodeadas por enormes muros para protegerse \n";
-		cout << " de los titanes, monstruos gigantes que devoran personas, mientras Eren Yeager y sus amigos luchan por descubrir \n";
-		cout << " la verdad detrC!s de estas criaturas y el origen de su mundo.\n";
-		break;
-	case 4:
-		cuestonario_manage();
-		break;
-	case 5:
-		cout << "\n Hasta luego!!! \n";
-		break;
-	default:
-		cout << "\n Opcion No Valida. Por favor, intente de nuevo.\n";
-		break;
-	}
-	// Pausa hasta que el usuario presione Enter
-	cout << "             \nPresione Enter para regresar al menu principal...";
-	cin.ignore(); // Ignora el salto de lC-nea pendiente
-	cin.get();    // Espera a que se presione Enter
+void mostrar_historia_onepiece() {
+    cout << "\nHistoria de One Piece:\n";
+    cout << "One Piece sigue a Monkey D. Luffy y su tripulacion de piratas en su busqueda por encontrar el legendario tesoro conocido como 'One Piece' para convertirse en el Rey de los Piratas. A lo largo de sus aventuras, Luffy forma fuertes lazos con su tripulacion y enfrenta enemigos poderosos.\n";
+    cout << "\n Presiona enter para continuar...\n";
+    cin.ignore();
+    cin.get();
 }
 
+void mostrar_historia_attack_on_titan() {
+    cout << "\nHistoria de Attack on Titan:\n";
+    cout << "En un mundo donde la humanidad vive protegida por enormes muros para defenderse de los titanes, gigantes humanoides que devoran a las personas, un joven llamado Eren Yeager se une al ejercito despues de que su ciudad natal fuera destruida por un titan. La historia sigue su lucha por la supervivencia y la revelacion de los secretos detras de los titanes.\n";
+    cout << "\n Presiona enter para volver al menu...\n";
+    cin.ignore();
+    cin.get();
+}
 
 int main() {
-	int opcion, score;
+    int opcion;
+    do {
+        mostrar_menu();
+        cin >> opcion;
 
-	mostrar_menu();
-	cin >> opcion;
+        switch (opcion) {
+        case 1:
+            cout << "\nSeleccione un anime para ver la historia:\n";
+            cout << "1. Naruto\n";
+            cout << "2. One Piece\n";
+            cout << "3. Attack on Titan\n";
+            cout << "4. Regresar al menu principal\n";
+            int opcion_historia;
+            cin >> opcion_historia;
 
-	while (opcion != 5) { //Mariana: (ARREGLAR)se necesita arreglar el bucle, ya que al querer volver al menu principal, y elegir opciones de este, vuelve a salir el menu/
-		opcion_menu(opcion);
-		mostrar_menu();
-		cin >> opcion;
-	}
+            switch (opcion_historia) {
+                case 1:
+                    mostrar_historia_naruto();
+                    break;
+                case 2:
+                    mostrar_historia_onepiece();
+                    break;
+                case 3:
+                    mostrar_historia_attack_on_titan();
+                    break;
+                case 4:
+                    break; // Regresar al menu principal
+                default:
+                    cout << "Opcion no valida. Regresando al menu principal...\n";
+                    break;
+            }
+            break;
+        case 2:
+            cout << "\nSeleccione un anime para comenzar el cuestionario:\n";
+            cout << "1. Naruto\n";
+            cout << "2. One Piece\n";
+            cout << "3. Attack on Titan\n";
+            cout << "4. Regresar al menu principal\n";
+            int opcion_cuestionario;
+            cin >> opcion_cuestionario;
 
-	cout << "\n Programa terminado. B!Gracias por participar!\n";
-	return 0;
+            switch (opcion_cuestionario) {
+                case 1:
+                    cuestionario_naruto();
+                    break;
+                case 2:
+                    cuestionario_onepiece();
+                    break;
+                case 3:
+                    cuestionario_attack_on_titan();
+                    break;
+                case 4:
+                    break; // Regresar al menu principal
+                default:
+                    cout << "Opcion no valida. Regresando al menu principal...\n";
+                    break;
+            }
+            break;
+        case 3:
+            cout << "Saliendo...\n";
+            break;
+        default:
+            cout << "Opcion no valida, por favor intente nuevamente.\n";
+        }
+
+    } while (opcion != 3);
+
+    return 0;
 }
